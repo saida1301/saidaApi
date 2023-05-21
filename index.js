852ff8c81816793eacb08b96b8fe133069d1952d
saida1301 <119286237+saida1301@users.noreply.github.com>
2023-05-21 00:24:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -695,18 +695,16 @@ app.get("/training/:userId", (req, res) => {
 
 
 app.post('/vacancies', async (req, res) => {
-  const { user_id, company_id,city_id, category_id, experience_id, education_id, position, min_salary, max_salary, min_age, max_age, requirement, description, contact_name, accept_type,job_type, deadline } = req.body;
-  const query = `INSERT INTO vacancies (user_id, company_id,city_id, category_id, experience_id, education_id, position, min_salary, max_salary, min_age, max_age, requirement, description, contact_name, accept_type,job_type, deadline, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?,?,?,?,?,?,?,?,?, NOW(), NOW())`;
-  const values = [user_id, company_id,city_id, category_id, experience_id, education_id, position, min_salary, max_salary, min_age, max_age, requirement, description, contact_name, accept_type, job_type, deadline];
+  try {
+    const vacancyData = req.body;
+    const query = 'INSERT INTO vacancies SET ?';
+    await db.query(query, vacancyData);
 
-  pool.query(query, values, (error, results) => {
-    if (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Error adding vacancie' });
-    } else {
-      res.status(201).json({ message: 'Vacancy added successfully' });
-    }
-  });
+    res.status(201).json({ message: 'Vacancy added successfully' });
+  } catch (error) {
+    console.error('Error adding vacancy:', error);
+    res.status(500).json({ error: 'Failed to add vacancy' });
+  }
 });
 
 
