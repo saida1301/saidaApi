62eb703656e8e3546d9e2a5d37425f92c0dc90bf
saida1301 <119286237+saida1301@users.noreply.github.com>
2023-07-18 17:35:48 +0400

    Update index.js

--- index.js
+++ index.js
@@ -702,6 +702,30 @@ app.get("/vacancies", async (req, res) => {
     res.sendStatus(500);
   }
 });
+
+app.get('/vacancy/:userId', [
+  param('userId').isNumeric().withMessage('Invalid userId'),
+], (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const userId = req.params.userId;
+
+  const sql = "SELECT * FROM cv WHERE user_id = ? AND status = '1'"; 
+  const values = [userId];
+
+  pool.query(sql, values, (error, results) => {
+    if (error) {
+      console.error(error);
+      return res.status(500).send("Error retrieving cv");
+    }
+
+    return res.json(results);
+  });
+});
+
 app.get('/vacancy/:userId', [
   param('userId').isNumeric().withMessage('Invalid userId'),
 ], (req, res) => {
