07a8b347bc2294b4f45580355c13a4404b3d6e99
saida1301 <119286237+saida1301@users.noreply.github.com>
2023-08-03 23:25:49 +0400

    Update index.js

--- index.js
+++ index.js
@@ -2651,7 +2651,7 @@ app.post('/candidates', cors(), async (req, res) => {
 
 app.post('/candidat/:user_id/:vacancy_id', cors(), async (req, res) => {
   const { user_id, vacancy_id } = req.params;
-  const { cvUrl } = req.body;
+  const { cvUrl } = req.file;
 
   try {
     // Validate the cvUrl if needed
