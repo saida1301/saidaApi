54f726ad1ab887fdc9521e720e0c34b58c4b5b5a
saida1301 <119286237+saida1301@users.noreply.github.com>
2023-08-03 22:58:00 +0400

    Update index.js

--- index.js
+++ index.js
@@ -2649,29 +2649,13 @@ app.post('/candidates', cors(), async (req, res) => {
   }
 });
 
-app.post('/candidate/:user_id/:vacancy_id', cors(), upload.single('cv'), async (req, res) => {
+app.post('/candidate/:user_id/:vacancy_id', cors(), async (req, res) => {
   const { user_id, vacancy_id } = req.params;
-  const cvFile = req.file;
+  const { cvUrl } = req.body;
 
   try {
-    // Upload files to storage service (implement uploadToBlobStorage function accordingly)
-    let cvUrl = null;
-
-    if (cvFile) {
-      // Validate the CV file (e.g., check file size, type)
-      // Your validation logic here
-
-      const fileContents = cvFile.buffer;
-      const extension = '.pdf'; // Assuming CV files are in PDF format
-
-      const fileName = `cv_${uuidv4().substring(0, 6)}${extension}`; // Generate a random file name
-
-      console.log('CV dosyası yüklemesi başlıyor...');
-      await saveFileToHosting(fileContents, fileName, 'cvs');
-      console.log('CV dosyası yükleme tamamlandı!');
-
-      cvUrl = `back/assets/images/cvs/${fileName}`;
-    }
+    // Validate the cvUrl if needed
+    // Your validation logic here
 
     const query =
       'INSERT INTO candidates (vacancy_id,  user_id, cv, created_at, updated_at) VALUES (?, ?, ?,  NOW(), NOW())';
@@ -2685,28 +2669,7 @@ app.post('/candidate/:user_id/:vacancy_id', cors(), upload.single('cv'), async (
         res.status(500).json({ message: 'Error adding CV' });
       } else {
         // Send email to user
-        const transporter = nodemailer.createTransport({
-          service: 'gmail',
-          auth: {
-            user: 'humbeteliyevaseide2001@gmail.com',
-            pass: 'nwudhimwttuqdzxv',
-          },
-        });
-
-        const mailOptions = {
-          from: req.body.email,
-          to: 'humbesaida@gmail.com',
-          subject: 'Candidate Added Successfully',
-          text: 'Your request has been added successfully. Thank you!',
-        };
-
-        transporter.sendMail(mailOptions, (error, info) => {
-          if (error) {
-            console.error('Error sending email:', error);
-          } else {
-            console.log('Email sent:', info.response);
-          }
-        });
+        // Your email sending logic here
 
         res.status(201).json({ message: 'Candidate added successfully' });
       }
@@ -2716,6 +2679,7 @@ app.post('/candidate/:user_id/:vacancy_id', cors(), upload.single('cv'), async (
     res.status(500).json({ message: 'Error uploading Candidate' });
   }
 });
+
 
 
 app.get('/candidates/:user_id', (req, res) => {
