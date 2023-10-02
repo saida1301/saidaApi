c67debaf279679093e0235ec6fe1b3c59ade9b31
saida1301 <119286237+saida1301@users.noreply.github.com>
2023-10-02 09:10:40 +0400

    Update index.js

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.post('/login', async (req, res) => {
+app.post('/login',cors(), async (req, res) => {
   const { email, password } = req.body;
 
   pool.query(
@@ -518,7 +518,7 @@ app.get('/get_company_data/:companyId', (req, res) => {
 // Import necessary modules and middleware
 
 app.post(
-  '/signup',
+  '/signup',cors(),
   [
     body('name').notEmpty(),
     body('surname').notEmpty(),
