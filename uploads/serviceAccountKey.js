1147a5bced26132429e1caa809aae40b74fca3c0
saida1301 <119286237+saida1301@users.noreply.github.com>
2023-10-03 14:15:14 +0400

    Create serviceAccountKey.js

---
+++ uploads/serviceAccountKey.js
@@ -0,0 +1,13 @@
+export default  {
+  "type": "service_account",
+  "project_id": "ismobile-99990",
+  "private_key_id": "472079cf9e03cf6bd3f9dd078673ceea6db1877d",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDOWe9lxhunT+7K\nPHjnzOSPBrdN51MuzzGjU8w2sZzPwAPwn4GGj5GBD0P99UiQ/1tMGtf5hdLXQP55\ne8IQfse3L1O9KxaSzKvLx38vV9iy7DZDwL2jKKVi5xNI12bTBRPjQ2QxvJOepvTS\nPk1RGSdXb1l9Ud4zK81FpUMJcCSEcf+Tk9LXs2pRZzd/C+iPTDuqS0R3s2rtu1kx\n8/tKwfcJt2dxeDx7Gwg7nzg7/MmjBPkpxcoVLq/LdXHWmQy4lVRWp7RaLcvXj0mk\n+ZRhbwRBY1aAb7bWrj2hqndrn+/1FuAPKaFaX0QZ7SpK6OyYbvwwqrnWy8yw+QQX\nk3gOLQu7AgMBAAECggEADabjO5X+6AkAm4OjB3RFniAhH3fRnTQFrNsTPykYCUyY\nViKPZLIkkRJ2pUYa43DpfuBQb/MxGZ70rF/fh8zYygnzsye3H3DdYCbWMhvQkTcn\nI0C3Vc86GzrTdUsUQ/C+5JcQTr69piOV6JTznjfDo7lmWnAApd0HgHc9t1Gihi/l\nVK2O1P84B+OIGmdPXjhV2kAN1kNZd5DR7ldGH7nuRw794kfaUrYgT18LHi6WJvGl\nC6fNFKb4xHy82rbPhMXykfiZyZxW0MzwbqBUb0K/hbfjsmGwxvoDmkL4VBTwutiu\n16zAywpY/PAiyStMThCrPSop5rm2bDIdAgwXw3NCBQKBgQD/8Vb/6bICNXF4vlhj\npK2grqvIUfCg56ari/CX/LT2Yuxwc8qtunZnzrvIWd2ARH6rOwVkZ1VYV1Qd3hBE\nhr7dwCuO7xzsS5zBfC2GzFg8rQAWjQGFHljsTpIArF31VRkWi11rSVlRLkfPEQZy\nyfoyiJYlgU/i9xesEIHh3n0kNwKBgQDOZcE3la8oiIvA+wLd2lngDtjR/KfzTz9Y\nyNikmLAFB/stPDhK2XQzTfBeOduQfAxXmRHaQIw3eXknXeayFANk7dGlBuHjqV1+\nc+nlR8BxNjWu/pigdvmtsNJibuyEB8EmGjkyRTp+0MYrxGTuurAvwubEozUNhXU5\nbxYf3KHanQKBgHQ444jSVkMqgZyS0xBsd2u/5cHRc7OdfdHoehiKaKJ5yWRHMIIz\nahE5j4wrQqo8cEjRRGGTiu3Suh/V9w0nIwUPfrgpXLjwnP4/pWzQ3kCvTpru7QHG\nBhtIkoQ7wW7mdb4HCyTs6eIzYehlksgNb9ACtivRzLUD/3vjAxfOMIWBAoGBAJUs\nmDzbc9Cg+BlPkp/UPBcjw/PpX14MjeVWrLYyIS5RuWnVV35JsqGOGEeSG65O6gC6\nAlwzQOSraDRauu1EZ7uf42EaG1OXSpqlnox3kpCAtMY4/wkjVIwD+9rm/wziKein\nFWXPAVs8+64QcT7WMZcjj/TZXPPb3IDZWC1JIrZFAoGASDqT5bYa/mgCCFFBYfsG\nh09b1R4NP7qCQcgJOKPsNXOMmdxyPNXe+B4Jt8AMcRshVxo/LmOldV5hz2RQP2Od\nq4R5TpObom+RRpNcdJ66+0swc1Xak7iBl/t/8lr7oLhiDog/B1EOyoVsUwWu8euH\nZA4jRDbCJ4AGauzCqgmpUoE=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-5y77l@ismobile-99990.iam.gserviceaccount.com",
+  "client_id": "101971513653274303192",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-5y77l%40ismobile-99990.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
